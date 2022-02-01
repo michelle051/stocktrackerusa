5e586c76039a6920fd614c456e8c3026abd5ddf1
권용오 <michelle05.kwon@gmail.com>
2022-02-02 07:15:42 +0900

    서버셋팅오류디버깅

--- config/settings/prod.py
+++ config/settings/prod.py
@@ -1,17 +1,6 @@
 from .base import *
 
-ALLOWED_HOSTS = ['15.165.138.67', 'djangostock.co.kr']
+ALLOWED_HOSTS = ['15.165.138.67']
 STATIC_ROOT = BASE_DIR / 'static/'
 STATICFILES_DIRS = []
 DEBUG = False
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'pybo',
-        'USER': 'dbmasteruser',
-        'PASSWORD': '05tkfkdgo!',
-        'HOST': 'ls-c152d9157d388ff408176e07d31a0d9370be7bb6.cmp3sco2upat.ap-northeast-2.rds.amazonaws.com',
-        'PORT': '5432'
-    }
-}
